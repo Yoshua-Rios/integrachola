05f6dde4a70dca63ca506ef51c69e3ad10d19314
Picazo27 <greciapicazoxd@gmail.com>
2023-11-27 16:28:07 -0600

    cambios5

---
+++ prototipo/src/components/botonAnadir.vue
@@ -0,0 +1,25 @@
+<template>
+<v-row>
+    
+    <v-btn @click="onButtonClick" size="30px">
+    <v-icon size="28px">mdi-plus</v-icon>
+</v-btn> 
+    <lalbel style="margin-left: 20px;">Añadir Producto</lalbel>
+</v-row>
+
+</template>
+
+
+<script>
+
+
+</script>
+
+<style>
+
+.v-row{
+    align-content: center;
+    justify-content: center;
+}
+
+</style>
