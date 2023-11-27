07447da9e1933278a153f99418ce787c45ad72c5
Yoshua <df5280135@gmail.com>
2023-11-26 20:40:03 -0600

    primeras vistas

---
+++ prueba/src/views/clienteS.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <v-row>
+           <v-col>
+            <v-row>
+                <h1 style="margin-bottom: 30px; margin-top: 40px;">Direcciones</h1>
+            </v-row>
+          <v-card  text="Datos Basicos" width="500px" margin="auto" >
+            <v-card-item>
+              Codigo Postal:
+            </v-card-item>
+            <v-card-item>
+              Colonia:
+            </v-card-item>
+            <v-card-item>
+              Calle:
+            </v-card-item>
+            <v-card-item>
+              Numero:
+            </v-card-item>
+          </v-card>
+           </v-col>
+           </v-row>
+    
+    </template>
+    
+    <script>
+    
+    
+    
+    </script>
+    
+    <style>
+    .v-col{
+        justify-content: center;
+    }
+    </style>
---
+++ prueba/src/views/miVista.vue
@@ -0,0 +1,22 @@
+<!-- MiVista.vue -->
+<template>
+    <v-container>
+      <h1>Esta es MiVista</h1>
+      <mi-componente></mi-componente> <!-- Aquí estás usando el componente -->
+    </v-container>
+  </template>
+  
+  <script>
+  import MiComponente from '@/components/miComponente.vue'; // Ajusta la ruta según tu estructura
+  
+  export default {
+    components: {
+      MiComponente
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos de la vista si es necesario */
+  </style>
+  
